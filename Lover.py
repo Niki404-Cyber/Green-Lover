d6d36f0f35d25ae2cad435bb3ae80413acc98f80
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2022-09-28 03:12:42 +0300

    Update Lover.py

--- Lover.py
+++ Lover.py
@@ -1,9 +1,5 @@
 import os, sys
-try:os.system('git pull')
-except:pass
-try:os.system('touch .prox.txt')
-except:pass
 try:
-    __import__("full").sex_but_oky()
+    __import__("full").__niki____()
 except Exception as e:
     exit(str(e))
